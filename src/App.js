c1cc69654099309124ba272e836ff1a99a007136
mahieyin-rahmun <mahieyin.rahmun@gmail.com>
2020-05-03 08:46:07 +0600

    initial UI setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { library } from '@fortawesome/fontawesome-svg-core'; // the core library
+import { fab } from '@fortawesome/free-brands-svg-icons';    // the icon library
+import { faHamburger } from '@fortawesome/free-solid-svg-icons';
+
+import AppNavBar from "./components/layouts/AppNavBar";
+
+library.add(fab, faHamburger);
 
 class App extends Component {
   render() {
     return (
-      <div>
-        Client Dues Panel
-      </div>
+      <Router>
+        <div>
+          <AppNavBar />
+          <div className="container container-fluid">
+            <h1>Client Dues Panel</h1>
+          </div>
+        </div>
+      </Router>
     )
   }
 }
