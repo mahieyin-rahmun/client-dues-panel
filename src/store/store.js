9227bf988ceef08c1a645ebc0760c3436701d1bf
mahieyin-rahmun <mahieyin.rahmun@gmail.com>
2020-05-03 14:37:16 +0600

    integration with firestore and redux

---
+++ src/store/store.js
@@ -0,0 +1,54 @@
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import { firebaseReducer } from 'react-redux-firebase';
+import { createFirestoreInstance, firestoreReducer } from 'redux-firestore';
+import firebaseConfig from "./FirebaseConfig";
+
+// Other Reducers
+// @TODO
+
+const rrfConfig = {
+    userProfile: 'users',
+    useFirestoreForProfile: true
+};
+
+// Initialize firebase instance
+firebase.initializeApp(firebaseConfig);
+
+// Add firebase to reducers, other reducers will also go here
+const rootReducer = combineReducers({
+    firebase: firebaseReducer,
+    firestore: firestoreReducer
+});
+
+// thunk middleware
+const middleware = [thunk];
+
+// Create store with reducers and initial state
+const initialState = {}
+const store = createStore(
+    rootReducer,
+    initialState,
+    compose(
+        applyMiddleware(...middleware),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+);
+
+// need this in ReactReduxFirebaseProvider
+const rrfProps = {
+    firebase,
+    config: rrfConfig,
+    dispatch: store.dispatch,
+    createFirestoreInstance
+};
+
+export {
+    store,
+    rrfProps
+};
+
