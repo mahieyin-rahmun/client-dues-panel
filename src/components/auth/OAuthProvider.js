9e2a7be58eb8b143b4448ff3a9268f511cae2753
mahieyin-rahmun <mahieyin.rahmun@gmail.com>
2020-05-04 17:15:46 +0600

    refactored Oauth Providers into separate component

---
+++ src/components/auth/OAuthProvider.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { firebaseConnect } from 'react-redux-firebase';
+
+import notifyUser from '../../store/actions/action-creators/NotifyUser';
+
+class OAuthProvider extends Component {
+	constructor() {
+		super();
+		this.onOauthProviderButtonClicked = this.onOauthProviderButtonClicked.bind(this);
+	}
+
+	onOauthProviderButtonClicked(provider, event) {
+		const { firebase } = this.props;
+
+		firebase.login({
+			provider: provider,
+			type: 'popup'
+		})
+			.then((res) => {
+				this.props.notifyUser({
+					messageType: "success",
+					body: ""
+				});
+			})
+			.catch((err) => {
+				this.props.notifyUser({
+					messageType: "error",
+					body: err.message
+				})
+			});
+	}
+
+	render() {
+		return (
+			<div>
+				<p className="text mt-4">Alternatively, you can sign in using one of the following providers: </p>
+				<button type="submit" className="btn btn-danger btn-large btn-block" onClick={this.onOauthProviderButtonClicked.bind(this, "google")}>
+					<FontAwesomeIcon className="mr-3" icon={["fab", "google"]}></FontAwesomeIcon> Sign In With Google
+							</button>
+				<button type="submit" className="btn btn-secondary btn-large btn-block" onClick={this.onOauthProviderButtonClicked.bind(this, "github")}>
+					<FontAwesomeIcon className="mr-3" icon={["fab", "github"]}></FontAwesomeIcon> Sign In With Github
+							</button>
+			</div>
+		)
+	}
+}
+
+const matchStateToProps = (state, props) => ({
+	notify: state.notify
+});
+
+export default compose(
+	firebaseConnect(),
+	connect(matchStateToProps, { notifyUser })
+)(OAuthProvider);
