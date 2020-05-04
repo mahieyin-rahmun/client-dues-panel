0704fa7e52902ea4bf68f158c8eb60651de59f97
mahieyin-rahmun <mahieyin.rahmun@gmail.com>
2020-05-04 14:27:09 +0600

    loading component for auth helper

---
+++ src/components/layouts/Loading.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Loading() {
+    return (
+        <div className="alert alert-info font-weight-bold text-center">
+            Authenticating...
+        </div>
+    )
+};
