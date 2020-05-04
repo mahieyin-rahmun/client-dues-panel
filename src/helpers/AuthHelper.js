0a0dd16a329d2787a381474c79de3f382c998bf6
mahieyin-rahmun <mahieyin.rahmun@gmail.com>
2020-05-04 14:27:33 +0600

    auth helper wrappers

---
+++ src/helpers/AuthHelper.js
@@ -0,0 +1,29 @@
+import locationHelperBuilder from 'redux-auth-wrapper/history4/locationHelper';
+import { connectedRouterRedirect } from 'redux-auth-wrapper/history4/redirect'
+import Loading from '../components/layouts/Loading';
+import * as AppRoutes from "../components/routes";
+const locationHelper = locationHelperBuilder({});
+
+export const UserIsAuthenticated = connectedRouterRedirect({
+    wrapperDisplayName: 'UserIsAuthenticated',
+    AuthenticatingComponent: Loading,
+    allowRedirectBack: true,
+    redirectPath: (state, ownProps) =>
+        locationHelper.getRedirectQueryParam(ownProps) || AppRoutes.LOGIN,
+    authenticatingSelector: ({ firebase: { auth, profile, isInitializing } }) =>
+        !auth.isLoaded || isInitializing === true,
+    authenticatedSelector: ({ firebase: { auth } }) =>
+        auth.isLoaded && !auth.isEmpty
+});
+
+export const UserIsNotAuthenticated = connectedRouterRedirect({
+    wrapperDisplayName: 'UserIsNotAuthenticated',
+    AuthenticatingComponent: Loading,
+    allowRedirectBack: false,
+    redirectPath: (state, ownProps) =>
+        locationHelper.getRedirectQueryParam(ownProps) || AppRoutes.DASHBOARD,
+    authenticatingSelector: ({ firebase: { auth, isInitializing } }) =>
+        !auth.isLoaded || isInitializing === true,
+    authenticatedSelector: ({ firebase: { auth } }) =>
+        auth.isLoaded && auth.isEmpty
+});
